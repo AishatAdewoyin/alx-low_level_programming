4a7d4b07dfc320655fed30d386abc17dce33890d
Aishat452 <kaltumenterprices4u@gmail.com>
2023-02-16 14:38:09 -0800

    last

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
  * return:void(0)
  */
 
-int main(void)
-/*This program prints without the printf function*/
+int main(void) /* This program prints without the printf function */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
