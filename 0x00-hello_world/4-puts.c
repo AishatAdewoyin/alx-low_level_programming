7b2da0a5e935bbc45afaaf2c887cc50ad4933f72
Aishat452 <kaltumenterprices4u@gmail.com>
2023-02-16 13:59:25 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * Created by Aishat. This program prints without the printf function
+ * Puts:Programming is like building a multilingual puzzle
+ * return:void(0)
+ */
+
+int main(void)
+{
+puts["\""Programming is like building a multilingual puzzle];
+ return(0);
+}	
