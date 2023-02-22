cda50b8ce4e57b520d69632c6420c0df9e64abe9
Aishat452 <aishadeperfectly@gmail.com>
2023-02-23 00:57:01 +0100

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+int i;
+
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+{printf("%d, ", i);
+}
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+printf("%d, ", i);
+}
+}
+printf("98\n");
+}
