013721dc5fb1b89c45e83215d2cae475492105d8
Aishat452 <aishadeperfectly@gmail.com>
2023-02-23 07:58:19 +0100

    new

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,15 +2,17 @@
 
 /**
  * main - Prints the sum of all the multiples of 3 or 5 below 1024
- * Retun: 0
+ * Return: 0
 */
 
 int main(void)
 {
 int sum = 0;
 int i;
-for (i = 0; i < 1024; i++) {
-if (i % 3 == 0 || i % 5 == 0) {
+for (i = 0; i < 1024; i++) 
+{
+if (i % 3 == 0 || i % 5 == 0) 
+{
 sum += i;
 }
 }
