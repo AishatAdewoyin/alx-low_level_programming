25f02cb256fdc5dd950d710dedd5001685f93f6d
Aishat452 <aishadeperfectly@gmail.com>
2023-02-23 08:01:15 +0100

    new commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,14 +9,14 @@ int main(void)
 {
 int sum = 0;
 int i;
-for (i = 0; i < 1024; i++) 
+for (i = 0; i < 1024; i++)
 {
-if (i % 3 == 0 || i % 5 == 0) 
+if (i % 3 == 0 || i % 5 == 0)
 {
 sum += i;
 }
 }
 printf("%d\n", sum);
-return 0;
+return (0);
 }
 
