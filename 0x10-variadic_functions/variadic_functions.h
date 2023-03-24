9ebec6e67eec809f5afa7fb86a0ac958010b5bd8
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-03-24 16:06:11 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,26 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char);
 
+/**
+ * struct print - A struct containing a character and its corresponding function
+ * for printing a va_list argument.
+ *
+ * @spec: A character representing the type of the va_list argument.
+ * @fn: A function pointer to a function that takes a va_list and prints a
+ * single argument of type @spec.
+ */
+typedef struct print
+{
+	char spec;
+	void (*fn)(va_list);
+} print_t;
+
+int _putchar(char c);
+void print_char(va_list arg);
+void print_integer(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
+
 #endif
