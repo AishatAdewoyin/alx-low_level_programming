c21b8d7207f622a77565648cfc89a33f7e95c5f8
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-03-31 01:27:36 +0100

    Linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,14 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 /**
- *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      *
- *       * Description: singly linked list node structure
- *        * for Holberton project
- *         */
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct list_s
 {
 	        char *str;
