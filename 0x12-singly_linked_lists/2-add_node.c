762398ca8031df062b0e292fa35c1f03362aacbe
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-03-31 01:22:51 +0100

    Linde lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,12 +2,12 @@ nclude <stdlib.h>
 #include <string.h>
 #include "lists.h"
 /**
- *  * add_node - adds a new node at the beginning of a linked list
- *   * @head: double pointer to the list_t list
- *    * @str: new string to add in the node
- *     *
- *      * Return: the address of the new element, or NULL if it fails
- *       */
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ * Return: the address of the new element, or NULL if it fails
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	 list_t *new;
