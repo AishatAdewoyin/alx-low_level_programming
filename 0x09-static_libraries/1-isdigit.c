bb0d6859ed84cf34483501972b66d2f762948319
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-05-01 11:17:55 +0100

    Updated

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,12 +1,26 @@
 #include "main.h"
-		
+
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - checks if a character is a digit (0-9)
+ * @c: the character to check
+ * Return: 1 if c is a digit, otherwise 0
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+	switch (c)
+	{
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			return (1);
+		default:
+			return (0);
+	}
 }
-
