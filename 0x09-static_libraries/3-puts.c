09a5ad71c1d0b1ce0f354671f856f36280e31900
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-05-01 12:17:05 +0100

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,15 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - Prints a string to the standard output.
+ * @str: Pointer to the string.
+ * Return: void
  */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
