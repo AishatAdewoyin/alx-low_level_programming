a66f533b955bc04fb1eb2c8abd7ce681d51eefc6
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-05-01 12:30:27 +0100

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,12 +1,19 @@
 #include "main.h"
-		
+
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ * Return: 1 if c is an alphabetic character, 0 otherwise
  */
+
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+    switch (c)
+    {
+        case 'a'...'z':
+        case 'A'...'Z':
+            return (1);
+        default:
+            return (0);
+    }
 }
-
