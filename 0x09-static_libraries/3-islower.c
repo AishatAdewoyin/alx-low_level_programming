3e6233fc18a218c769c9dbea80695cf522cf158f
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-05-01 12:12:17 +0100

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _islower - a function that checks for lowercase
+ * @c: the character
+ * Return: 1 if lowercase, else 0.
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
+	return (c >= 'a' && c <= 'z') ? 1 : 0;
 }
