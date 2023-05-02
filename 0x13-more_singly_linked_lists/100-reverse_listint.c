794b7ed99713996ab83bf4784874dda2c9319c07
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-05-02 03:31:52 +0100

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,29 +9,25 @@
  * Return: If the function succeeds, the address of the new node.
  *         Otherwise, NULL.
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+	
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *new_node, *current = *head;
-	unsigned int i;
+    listint_t *prev = NULL, *next;
 
-	if (head == NULL)
-		return (NULL);
+    if (*head == NULL || (*head)->next == NULL)
+        return (*head);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
+    while ((*head) != NULL)
+    {
+        next = (*head)->next;
+        (*head)->next = prev;
+        prev = (*head);
+        (*head) = next;
+    }
+    *head = prev;
+    return (*head);
+}
 
-	new_node->n = n;
-
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-
-	for (i = 0; i < idx - 1 && current != NULL; i++)
-		current = current->next;
 
 	if (current == NULL)
 	{
