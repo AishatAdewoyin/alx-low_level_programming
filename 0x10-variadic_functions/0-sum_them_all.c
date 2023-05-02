c49104cec1c0ed2a0602d25dd48256f620788850
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-05-02 12:38:52 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,6 +10,7 @@
 */
 
 int sum_them_all(const unsigned int n, ...)
+
 {
 va_list args;
 int sum = 0;
@@ -18,7 +19,6 @@ if (n == 0)
 return (0);
 va_start(args, n);
 for (i = 0; i < n; i++)
-{
 sum += va_arg(args, int);
 va_end(args);
 return (sum);
