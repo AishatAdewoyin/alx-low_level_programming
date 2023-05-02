a71ee4c463d3ceea6d186644b6d53d2f14874c39
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-05-02 10:40:36 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * str_concat - get ends of input and add together for size	
- * @s1: input one to concat	
- * @s2: input two to concat	
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
  * Return: concat of s1 and s2
  */
 
