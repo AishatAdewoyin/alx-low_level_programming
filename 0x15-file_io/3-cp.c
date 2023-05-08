fb752e464dcf23512a95f45b8301dd42b272bd9c
Aishat452 <aishadeperfectly@gmail.com>
2023-05-08 09:02:02 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,44 +18,55 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(EXIT_FAILURE);
 	}
+
 	fileFro = open(argv[1], O_RDONLY);
 	if (fileFro == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
 	fileTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fileTo == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fileFro);
 		exit(EXIT_FAILURE);
 	}
+
 	do {
 		readByte = read(fileFro, buffer, BUF_TO_READ_FROM_INPUT);
 		if (readByte == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(fileFro);
+			close(fileTo);
 			exit(EXIT_FAILURE);
 		}
+
 		writeByte = write(fileTo, buffer, readByte);
 		if (writeByte == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fileFro);
+			close(fileTo);
 			exit(EXIT_FAILURE);
 		}
 	} while (readByte > 0);
 
-closeFro = close(fileFro);
-if (closeFro == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFro);
-	exit(EXIT_FAILURE);
-}
+	closeFro = close(fileFro);
+	if (closeFro == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFro);
+		exit(EXIT_FAILURE);
+	}
+
 	closeTo = close(fileTo);
-if (closeTo == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo);
-	exit(EXIT_FAILURE);
-}
+	if (closeTo == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo);
+		exit(EXIT_FAILURE);
+	}
+
 	return (0);
 }
