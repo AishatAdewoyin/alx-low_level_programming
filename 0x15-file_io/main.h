af2f43ecd7e904a2c4f6d4536b1f7acd18714ea5
Aishat452 <aishadeperfectly@gmail.com>
2023-05-10 18:20:10 -0700

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,15 @@ int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char);
 int close(int);
 
+
+void check_elf(const unsigned char *e_ident);
+void print_magic(const unsigned char *e_ident);
+void print_class(const unsigned char *e_ident);
+void print_data(const unsigned char *e_ident);
+void print_version(const unsigned char *e_ident);
+void print_abi(const unsigned char *e_ident);
+void print_osabi(const unsigned char *e_ident);
+void print_type(unsigned int e_type, const unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, const unsigned char *e_ident);
+void close_elf(int elf);
 #endif
