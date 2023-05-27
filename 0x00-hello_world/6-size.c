b4a7cde09c556403accd2482ba6e997be2a9cd5e
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-05-27 06:22:08 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,13 @@
 * main - Entry point
 * Description: this program prints the size of
 * a data type in bytes
+* size_t: represents sizes of object in memory
+* just to remember
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
