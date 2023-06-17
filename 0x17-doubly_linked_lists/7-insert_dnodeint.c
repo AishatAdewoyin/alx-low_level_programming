7064592129ca717181cb6a110558f69980958313
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-06-17 01:49:04 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,28 +16,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     unsigned int i;
 
     if (h == NULL)
-        return NULL;
+        return (NULL);
 
     if (idx == 0)
-        return add_dnodeint(h, n);
+        return (add_dnodeint(h, n));
 
     current_node = *h;
     for (i = 0; i < idx - 1; i++)
     {
         if (current_node == NULL)
-            return NULL;
+            return (NULL);
         current_node = current_node->next;
     }
 
     if (current_node == NULL)
-        return NULL;
+        return (NULL);
 
     if (current_node->next == NULL)
-        return add_dnodeint_end(h, n);
+        return (add_dnodeint_end(h, n));
 
     new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
-        return NULL;
+        return (NULL);
 
     new_node->n = n;
     new_node->prev = current_node;
@@ -45,5 +45,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     current_node->next->prev = new_node;
     current_node->next = new_node;
 
-    return new_node;
+    return (new_node);
 }
