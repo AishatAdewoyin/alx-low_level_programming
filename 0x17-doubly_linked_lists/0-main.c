70b8bff6a819f7df9e3774103c9f599e032022e7
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-06-17 02:20:33 +0100

    Update 0-main.c

--- 0x17-doubly_linked_lists/0-main.c
+++ 0x17-doubly_linked_lists/0-main.c
@@ -3,6 +3,11 @@
 #include<stdio.h>
 #include"lists.h"
 
+/**
+ * main - Entry point of the program.
+ * Return: EXIT_SUCCESS if successful,
+ * EXIT_FAILURE otherwise.
+ */
 int main(void)
 {
 	dlistint_t *head;
@@ -11,19 +16,26 @@ int main(void)
 	size_t n;
 
 	head = &hello;
+
+	/* Allocate memory for a new node */
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		dprintf(2, "Error: Can't malloc\n");
 		return (EXIT_FAILURE);
 	}
+
 	new->n = 9;
 	head->prev = new;
 	new->next = head;
 	new->prev = NULL;
 	head = new;
+
+	/* Print the elements in the doubly linked list */
 	n = print_dlistint(head);
 	printf("->%lu elements\n", n);
+
 	free(new);
+
 	return (EXIT_SUCCESS);
 }
