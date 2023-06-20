54c7ca0ff5b231dfcdfaa57842c65680e63d30af
Aishat452 <aishadeperfectly@gmail.com>
2023-06-20 12:29:00 -0700

    more functions nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
-*/
-
+ * print_most_numbers - Prints the numbers from 0 to 9, excluding 2 and 4,
+ *                      followed by a new line.
+ */
 void print_most_numbers(void)
 {
- int c;
+	int c;
 
- for (c = 48; c < 58; c++)
- {
- if (c != 50)
- {
- if (c != 52)
- {
- _putchar(c);
- }
- }
- }
- _putchar('\n');
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,36 +2,39 @@
 #include <stdio.h>
 
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
+ * main - Prints the numbers from 1 to 100, with some conditions.
+ *
+ * Description: Prints numbers from 1 to 100, but for multiples of three
+ *              prints "Fizz", for multiples of five prints "Buzz",
+ *              and for multiples of both three and five prints "FizzBuzz".
+ *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-int i;
+	int i;
 
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 != 0)
-{
-printf(" Fizz");
-} else if (i % 5 == 0 && i % 3 != 0)
-{
-printf(" Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
-{
-printf(" FizzBuzz");
-} else if (i == 1)
-{
-printf("%d", i);
-} else
-{
-printf(" %d", i);
-}
-}
-printf("\n");
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
 
-return (0);
+	printf("\n");
+
+	return (0);
 }
