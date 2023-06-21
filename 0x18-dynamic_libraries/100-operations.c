5de3bd0faa18a1e1c1e32cce6f8ac4242f71d003
Aishat Adeladun Adewoyin <96025207+Aishat452@users.noreply.github.com>
2023-06-21 10:22:56 +0100

    Update 100-operations.c

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The sum of the two integers
+ */
+int add(int a, int b)
+{
+    return a + b;
+}
+
+/**
+ * sub - Subtracts two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The difference between the two integers
+ */
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The product of the two integers
+ */
+int mul(int a, int b)
+{
+    return a * b;
+}
+
+/**
+ * divide - Divides two integers
+ * @a: The numerator
+ * @b: The denominator
+ *
+ * Return: The quotient of the division
+ */
+int divide(int a, int b)
+{
+    return a / b;
+}
+
+/**
+ * mod - Computes the modulus of two integers
+ * @a: The dividend
+ * @b: The divisor
+ *
+ * Return: The remainder of the division
+ */
+int mod(int a, int b)
+{
+    return a % b;
+}
